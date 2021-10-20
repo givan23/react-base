6d92221ac616161d5eec5ce16e782309ea53d1ec
ivantrabucco <ivan.trabucco@accenture.com>
2021-10-20 18:13:10 +0200

    prova commit

--- src/components/default/default.component.tsx
+++ src/components/default/default.component.tsx
@@ -3,26 +3,26 @@ import React from "react";
 import useDefault from "./useDefault";
 
 const DefaultComponent = () => {
-  const { settings, value, getData, getIncrement, getDecrement } = useDefault();
-  return (
-    <div>
-      {/* <img src={dog} alt="" /> */}
-      <button data-testid="data-btn" onClick={getData}>
-        GET DATA
-      </button>
-      <p data-testid="data-response">{settings?.response}</p>
-      <br />
-      <br />
-      <button data-testid="increment-btn" onClick={getIncrement}>
-        INCREMENT
-      </button>
-      <button data-testid="decrement-btn" onClick={getDecrement}>
-        DECREMENT
-      </button>
-      <h1 data-testid="value-response">{value}</h1>
-    </div>
-  );
+    const {settings, value, getData, getIncrement, getDecrement} = useDefault();
+    return (
+        <div>
+            {/* <img src={dog} alt="" /> */}
+            <button data-testid="data-btn" onClick={getData}>
+                GET DATA
+            </button>
+            <p data-testid="data-response">{settings?.response}</p>
+            <br/>
+            <br/>
+            <button data-testid="increment-btn" onClick={getIncrement}>
+                INCREMENT
+            </button>
+            <button data-testid="decrement-btn" onClick={getDecrement}>
+                DECREMENT
+            </button>
+            <h1 data-testid="value-response">{value}</h1>
+        </div>
+    );
 };
-
+/*pippo*/
 export default DefaultComponent;
 
